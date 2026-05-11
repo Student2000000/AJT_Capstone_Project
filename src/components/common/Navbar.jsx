2ce8b1d6577138000f1c035eb1d4f369b265902a
Jay A <Jenna.Arends@Seattlecolleges.edu>
2026-05-10 20:47:34 -0700

    It was bc I put brackets but no props, smh

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Grid, Button, Box, Text } from '@mantine/core'
 
-function Navbar({}) {
+function Navbar() {
     return(
         // pb="md" just makes the spacing match the style in theme.spcing
         <Box pb="md">
@@ -8,15 +8,14 @@ function Navbar({}) {
             <Text size="sm" fw={500} ta="left">
                 Filter by category:
             </Text>
-            
+
             {/*manually construted a grid of buttons */}
             <Grid>
-                <Grid.Col span={3.5}><Button fullWidth>Apperal</Button></Grid.Col>
+                <Grid.Col span={3.5}><Button fullWidth>Apparel</Button></Grid.Col>
                 <Grid.Col span={4}><Button fullWidth>Accessories</Button></Grid.Col>
                 <Grid.Col span={3.5}><Button fullWidth>Stationery</Button></Grid.Col>
             </Grid>
         </Box>
     )
 } 
-
 export default Navbar;
