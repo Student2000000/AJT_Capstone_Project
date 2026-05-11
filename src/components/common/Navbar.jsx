51e8991db48a67408c105ccec8c496365e01f30a
Jay A <Jenna.Arends@Seattlecolleges.edu>
2026-05-10 20:33:35 -0700

    Attempting lint fix

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -8,16 +8,14 @@ function Navbar({}) {
             <Text size="sm" fw={500} ta="left">
                 Filter by category:
             </Text>
-
+            
             {/*manually construted a grid of buttons */}
             <Grid>
                 <Grid.Col span={3.5}><Button fullWidth>Apperal</Button></Grid.Col>
                 <Grid.Col span={4}><Button fullWidth>Accessories</Button></Grid.Col>
                 <Grid.Col span={3.5}><Button fullWidth>Stationery</Button></Grid.Col>
             </Grid>
-
         </Box>
-
     )
 } 
 
