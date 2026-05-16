d8c7b2d88e233634a6b7c32dafb6bce2b4ffc60a
Jay A <Jenna.Arends@Seattlecolleges.edu>
2026-05-15 23:02:03 -0700

    moved code for catagory filtering from navbar file to category filter file

---
+++ src/components/common/CategoryFilter.jsx
@@ -0,0 +1,21 @@
+import { Grid, Button, Box, Text } from '@mantine/core'
+
+function Navbar() {
+    return(
+        // pb="md" just makes the spacing match the style in theme.spcing
+        <Box pb="md">
+            {/*Just text, nothing special*/} 
+            <Text size="sm" fw={500} ta="left">
+                Filter by category:
+            </Text>
+
+            {/*manually construted a grid of buttons */}
+            <Grid>
+                <Grid.Col span={3.5}><Button fullWidth>Apparel</Button></Grid.Col>
+                <Grid.Col span={4}><Button fullWidth>Accessories</Button></Grid.Col>
+                <Grid.Col span={3.5}><Button fullWidth>Stationery</Button></Grid.Col>
+            </Grid>
+        </Box>
+    )
+} 
+export default Navbar;
